c26d86abd8c9c9f57f2e6342ae6607fd2a843edb
Arthur Arnoux <arthur.arnx@gmail.com>
2023-11-11 16:21:00 +0100

    refactor

--- pphist.py
+++ pphist.py
@@ -9,7 +9,7 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-imgs = unpickle("../cifar-10-python/cifar-10-batches-py/data_batch_1")
+dataset = unpickle("../cifar-10-python/cifar-10-batches-py/data_batch_1")
 
 # todo remove found neighbor
 def get_similar_neighbor_in_range(histogram1: list, histogram2: list, index: int, wideness: int) -> int:
@@ -31,38 +31,40 @@ def to_grayscale(image: list) -> list:
         output[i] = int(image[i] * 0.299 + image[i+1024] * 0.587 + image[i+2048] * 0.114)
     return output
 
-print(imgs.keys())
-nb_bins = 25
-wanted_label = 8
-#hist, bins = np.histogram(imgs[b'data'][0], bins=np.arange(255))
+def extract_images_from_label(imgs: dict, wanted_label: str) -> list:
+    matching_imgs = []
+    for i, label in enumerate(imgs[b'labels']):
+        if label == wanted_label:
+            matching_imgs.append(imgs[b'data'][i])
+    return matching_imgs
 
-f, axarr = plt.subplots(2,1)
-
-hist1 = np.zeros(nb_bins)
-c = 0
-for i, label in enumerate(imgs[b'labels'][:5000]):
-    if label == wanted_label:
-        gray = to_grayscale(imgs[b'data'][i])
+def images_to_histogram(imgs: list, nb_bins: int) -> list:
+    hist1 = np.zeros(nb_bins)
+    c = 0
+    for img in imgs:
+        gray = to_grayscale(img)
         hist = np.histogram(gray, bins=nb_bins)
         hist1 += hist[0]
         c += 1
+    hist1 = np.divide(hist1, c)
+    return hist1
 
-hist1 = np.divide(hist1, c)
+nb_bins = 25
+wanted_label = 8
+f, axarr = plt.subplots(2,1)
+
+imgs = extract_images_from_label(dataset, wanted_label)
+middle = len(imgs)//2
+batch1 = imgs[:middle]
+batch2 = imgs[middle:]
+
+print(len(imgs))
+
+hist1 = images_to_histogram(batch1, nb_bins)
 axarr[0].hist(hist1, bins=nb_bins)
 
-
-hist2 = np.zeros(nb_bins)
-c = 0
-for i, label in enumerate(imgs[b'labels'][5000:]):
-    if label == wanted_label:
-        gray = to_grayscale(imgs[b'data'][i])
-        hist = np.histogram(gray, bins=nb_bins)
-        hist2 += hist[0]
-        c += 1
-
-hist2 = np.divide(hist2, c)
+hist2 = images_to_histogram(batch2, nb_bins)
 axarr[1].hist(hist2, bins=nb_bins)
-
 
 total_diff = 0
 for i, val in enumerate(hist1):
